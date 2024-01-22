d66fb243bbd7229f5f453b25efa3c591c2b35aeb
masanovsky <96916252+masanovsky@users.noreply.github.com>
2024-01-22 20:26:57 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -13,8 +13,8 @@ u8 = encoding.UTF8
 
 update_state = false
 
-local script_vers = 3
-local script_vers_text = "1.05"
+local script_vers = 5
+local script_vers_text = "5.05"
 
 local update_url = "https://raw.githubusercontent.com/thechampguess/scripts/master/update.ini" -- тут тоже свою ссылку
 local update_path = getWorkingDirectory() .. "/update.ini" -- и тут свою ссылку
